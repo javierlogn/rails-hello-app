a652d085c4915b75c75f3a1015fd3fce895fcf70
Javier Jara <javier.jara@logn.co>
2017-08-25 14:52:20 -0600

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello 
+  	render html: "hello, world❗️"
+  end
+
+  def goodbye
+  	render html: "Good Bye asshole 🖕"
+  end
+end
